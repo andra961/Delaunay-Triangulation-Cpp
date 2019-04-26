aaf52a37919aace7a4bdcd9faf207c6e73705ca7
andrea corronca <andrac1996@gmail.com>
2019-04-26 18:58:44 +0200

    miglioramento triangle2d

--- data_structures/triangle2d.cpp
+++ data_structures/triangle2d.cpp
@@ -1,18 +1,55 @@
 #include "triangle2d.h"
 
+/**
+ * @brief default constructor
+ */
+
 Triangle2d::Triangle2d()
 {
 
 }
 
+/**
+ * @brief constructor given the points
+ * @param points array containing 3 points
+ * @param clockWiseOrAntiClockWise if true the triangle must be in clockwise order else in anticlockwise order
+ */
+
 Triangle2d::Triangle2d(std::array<cg3::Point2Dd,3>& points,bool clockWiseOrAntiClockWise){
 
     if (clockWiseOrAntiClockWise){
-        sortClockWiseOrder(points);
+        sortTriangleClockWise(points);
     }
     else {
-        sortAntiClockWiseOrder(points);
+        sortTriangleAntiClockWise(points);
     }
 
-    this->vertexes=points;
+    this->vertices=points;
+}
+
+/**
+ * @return the points of the triangle
+ */
+
+const std::array<cg3::Point2Dd,3>& Triangle2d::getPoints() const{
+    return this->vertices;
+}
+
+/**
+ * @brief set the points of the triangle
+ * @param points array of 3 points that will overwrite the current 3 ones
+ * @param clockWiseOrAntiClockWise if true the triangle must be in clockwise order else in anticlockwise order
+ */
+
+void Triangle2d::setPoints(std::array<cg3::Point2Dd,3>& points,bool clockWiseOrAntiClockWise){
+
+    if (clockWiseOrAntiClockWise){
+        sortTriangleClockWise(points);
+    }
+    else {
+        sortTriangleAntiClockWise(points);
+    }
+
+
+    this->vertices = points;
 }
--- data_structures/triangle2d.h
+++ data_structures/triangle2d.h
@@ -5,17 +5,21 @@
 #include <cg3/geometry/2d/point2d.h>
 #include <algorithms/triangle_utilities.h>
 
+/**
+ * @brief class that represents a triangle;contains an array of 3 points ordered in clockwise or anticlockwise order
+ */
+
 class Triangle2d
 {
 public:
     Triangle2d();
     Triangle2d(std::array<cg3::Point2Dd,3>& points,bool clockWiseOrAntiClockWise);
 
-    //const std::array<cg3::Point2Dd,3> getPoints() const;
-    //void setPoints(const std::array<cg3::Point2Dd,3>);
+    const std::array<cg3::Point2Dd,3>& getPoints() const;
+    void setPoints(std::array<cg3::Point2Dd,3>& points,bool clockWiseOrAntiClockWise);
 
 protected:
-    std::array<cg3::Point2Dd,3> vertexes;
+    std::array<cg3::Point2Dd,3> vertices;
 };
 
 
